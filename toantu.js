08d7adaeb0479b40164cacfe81c7f8f17016bdc6
doantung33 <72287177+doantung33@users.noreply.github.com>
2020-10-08 11:04:45 +0700

    first git

---
+++ toantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhập chiều rông");
+inputHeight = prompt("Nhập chiều dài");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("the area is "+ area)
